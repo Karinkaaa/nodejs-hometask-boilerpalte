5b3be00673dcf48b0bfd0096fc1674244680a7ab
karyna <9548026git*>
2021-12-25 14:02:18 +0200

    Implemented middleware that returns result of the query

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,6 +1,25 @@
 const responseMiddleware = (req, res, next) => {
-   // TODO: Implement middleware that returns result of the query
+    if (res.data) {
+        res.send(res.data);
+    } else {
+        res.status(res.err.status).send(getErrorObject(res.err.message));
+    }
     next();
+};
+
+const getErrorObject = (message) => {
+    return {
+        error: true,
+        message
+    };
+};
+
+class ResponseError extends Error {
+    constructor(message, status) {
+        super(message);
+        this.status = status;
+    }
 }
 
 exports.responseMiddleware = responseMiddleware;
+exports.ResponseError = ResponseError;
