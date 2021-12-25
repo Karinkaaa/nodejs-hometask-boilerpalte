1b2948e580dccd171764ebf089157c6ba1c2874f
karyna <9548026git*>
2021-12-25 14:04:18 +0200

    Implemented methods to work with fighters

--- services/fighterService.js
+++ services/fighterService.js
@@ -1,7 +1,33 @@
-const { FighterRepository } = require('../repositories/fighterRepository');
+const { FighterRepository } = require("../repositories/fighterRepository");
 
 class FighterService {
-    // TODO: Implement methods to work with fighters
+    getAll() {
+        return FighterRepository.getAll();
+    }
+
+    search(search) {
+        const item = FighterRepository.getOne(search);
+        if (!item) {
+            return null;
+        }
+        return item;
+    }
+
+    create(fighter) {
+        return FighterRepository.create({ health: 100, ...fighter });
+    }
+
+    update(id, fighter) {
+        return FighterRepository.update(id, fighter);
+    }
+
+    delete(id) {
+        const items = FighterRepository.delete(id);
+        if (!items.length) {
+            return null;
+        }
+        return items[0];
+    }
 }
 
 module.exports = new FighterService();
