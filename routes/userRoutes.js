59dba639319e5f5a9f0b93fb3890799ecb805164
karyna <9548026git*>
2021-12-25 14:11:55 +0200

    Implemented controllers for user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,49 @@
-const { Router } = require('express');
-const UserService = require('../services/userService');
-const { createUserValid, updateUserValid } = require('../middlewares/user.validation.middleware');
-const { responseMiddleware } = require('../middlewares/response.middleware');
+const { Router } = require("express");
+const UserService = require("../services/userService");
+const { createUserValid, updateUserValid } = require("../middlewares/user.validation.middleware");
+const { responseMiddleware, ResponseError } = require("../middlewares/response.middleware");
 
 const router = Router();
 
-// TODO: Implement route controllers for user
+router.get("/", (req, res, next) => {
+    res.data = UserService.getAll();
+    next();
+}, responseMiddleware);
+
+router.get("/:id", (req, res, next) => {
+    const user = UserService.search(req.params);
+
+    if (user) {
+        res.data = user;
+    } else {
+        res.err = new ResponseError("User not found", 404);
+    }
+    next();
+}, responseMiddleware);
+
+router.post("/", createUserValid, (req, res, next) => {
+    if (res.err) return next();
+
+    res.data = UserService.create(req.body);
+    next();
+}, responseMiddleware);
+
+router.put("/:id", updateUserValid, (req, res, next) => {
+    if (res.err) return next();
+
+    res.data = UserService.update(req.params.id, req.body);
+    next();
+}, responseMiddleware);
+
+router.delete("/:id", (req, res, next) => {
+    const user = UserService.delete(req.params.id);
+
+    if (user) {
+        res.data = user;
+    } else {
+        res.err = new ResponseError("User not found", 404);
+    }
+    next();
+}, responseMiddleware);
 
 module.exports = router;
